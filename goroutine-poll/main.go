0f3a563c307169d5b78acdce3483fd73c9f3f474
Hadar Greinsmark <hadarg@kth.se>
2017-12-23 16:13:54 -0800

    Add Go poll example

---
+++ goroutine-poll/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"log"
+	"net"
+	"bytes"
+	"golang.org/x/sys/unix"
+	"os"
+)
+
+func main() {
+	addr, err := net.ResolveTCPAddr("tcp", ":7777")
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	replierPoll(listener)
+}
+
+func replierGoroutine(listener net.Listener) {
+	for {
+		conn, _ := listener.Accept()
+		go func() {
+			buf := make([]byte, 16)
+			conn.Read(buf)
+			log.Printf("received: %s", buf)
+			conn.Write(bytes.ToUpper(buf))
+			conn.Close()
+		}()
+	}
+}
+
+type ConnState struct {
+	connFile *os.File
+	buffer   []byte
+}
+
+func replierPoll(listener *net.TCPListener) {
+
+	epollFd, err := unix.EpollCreate(8)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// UNIX represents a TCP listener socket as a file
+	listenerFile, err := listener.File()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Add the TCP listener to the set of file descriptors being polled
+	listenerPoll := unix.EpollEvent{
+		Fd:     int32(listenerFile.Fd()),
+		Events: unix.POLLIN, // POLLIN triggers on accept()
+		Pad: 0, // Arbitary data
+	}
+	err = unix.EpollCtl(epollFd, unix.EPOLL_CTL_ADD, int(listenerPoll.Fd), &listenerPoll)()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Map EpollEvent.Pad to the connection state
+	states := map[int]ConnState{}
+
+	for {
+		// Wait infinitely until at least one new event is happening
+		var events []unix.EpollEvent
+		unix.EpollWait(epollFd, events, -1)
+
+		for _, event := range events {
+			if event.Fd == listenerPoll.Fd {
+				
+				// AcceptTCP() will now return immediately
+				conn, err := listener.AcceptTCP()
+				log.Printf("ee %v",err)
+				connFile, _ := conn.File()
+				conn.Close() // Close this an use the connFile copy instead
+
+				// Equal to starting the goroutine
+				newState := ConnState{
+					connFile: connFile,
+					buffer: make([]byte, 16),
+				}
+				fd := int(connFile.Fd())
+				states[fd] = newState
+
+				connPoll := unix.EpollEvent{
+					Fd:     int32(fd),
+					Events: unix.POLLIN, // POLLIN triggers on accept()
+					Pad: int32(fd), // So we can find states[fd] when triggered
+				}
+				unix.EpollCtl(epollFd, unix.EPOLL_CTL_ADD, fd, &connPoll)
+				continue
+			}
+
+			fd := int(event.Pad)
+			state := states[fd]
+
+			if event.Events == unix.POLLIN {
+				state.connFile.Read(state.buffer)
+				log.Printf("received: %s", state.buffer)
+
+				// Equal to switching away the goroutine
+				newPoll := event
+				newPoll.Events = unix.POLLOUT
+				unix.EpollCtl(epollFd, unix.EPOLL_CTL_MOD, fd, &newPoll)
+
+			} else if event.Events == unix.POLLOUT {
+				state.connFile.Write(bytes.ToUpper(state.buffer))
+				state.connFile.Close()
+
+				// Equal to stopping the goroutine
+				unix.EpollCtl(epollFd, unix.EPOLL_CTL_DEL, fd, nil)
+				delete(states, fd)
+			}
+		}
+	}
+}
